9d787bd065e5f696c29ca25e460e5670a48c6057
willmil11 <willmil1110@gmail.com>
2024-11-25 21:04:48 +0100

    Auto reset after build and run on normal exit (control + c)

--- os/buildandrun.sh
+++ os/buildandrun.sh
@@ -1,5 +1,4 @@
 echo "This script requires gcc, nasm, and qemu-system to run."
-echo "Please run the reset.sh script after running this one :)"
 echo "--"
 echo "Building..."
 nasm -f bin boot.asm -o boot.bin
@@ -11,3 +10,4 @@ echo "Verifying kernel offset..."
 xxd os.img | grep -A 10 "0000200"   # Check if kernel starts at Sector 2
 echo "Running..."
 qemu-system-i386 -drive format=raw,file=os.img
+bash reset.sh
