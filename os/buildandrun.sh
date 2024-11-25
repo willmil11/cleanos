ae6f05a8abe4d13ba0eecbd5c765fe360cb729f8
willmil11 <willmil1110@gmail.com>
2024-11-25 21:05:17 +0100

    Removing annoying check

--- os/buildandrun.sh
+++ os/buildandrun.sh
@@ -7,7 +7,6 @@ ld -m elf_i386 -Ttext 0x1000 kernel.o --oformat binary -o kernel.bin
 cat boot.bin kernel.bin > os.img
 echo "Built"
 echo "Verifying kernel offset..."
-xxd os.img | grep -A 10 "0000200"   # Check if kernel starts at Sector 2
 echo "Running..."
 qemu-system-i386 -drive format=raw,file=os.img
 bash reset.sh
